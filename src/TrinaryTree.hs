c8f1383bcaeb8a3f832123c441f63dcf32ce997c
Devon Stewart <blast@hardchee.se>
2014-12-07 12:30:54 -0800

    TrinaryTree: Initial commit

---
+++ src/TrinaryTree.hs
@@ -0,0 +1,30 @@
+module TrinaryTree where
+
+import Control.Applicative
+
+data TrinaryTree a = TEmpty | TTree a (TrinaryTree a) (TrinaryTree a) (TrinaryTree a)
+
+instance Functor TrinaryTree where
+  fmap _ TEmpty = TEmpty
+  fmap f (TTree x left center right) = TTree (f x) (f <$> left) (f <$> center) (f <$> right)
+
+instance Show a => Show (TrinaryTree a) where
+  show TEmpty = "TEmpty"
+  show (TTree x left center right) =
+    "TTree " ++ show x ++ "\n" ++
+      "|- " ++ (shift $ show left) ++
+      "|- " ++ (shift $ show center) ++
+      "|- " ++ (shift $ show right)
+    where shift s = unlines $ fmap ("  " ++) $ lines s
+
+insert :: Ord a => a -> TrinaryTree a -> TrinaryTree a
+insert x TEmpty = TTree x TEmpty TEmpty TEmpty
+insert y (TTree x left center right)
+  | y <  x = TTree x (work left) center right
+  | y == x = TTree x left (work center) right
+  | y >  x = TTree x left center (work right)
+  where work = insert y
+
+main = do
+  let x = foldl (flip insert) TEmpty [5, 4, 9, 5, 7, 2, 2]
+  putStrLn $ show x
