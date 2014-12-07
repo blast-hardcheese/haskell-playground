3df73910cce696903421a8485381ef2960e4d596
Devon Stewart <blast@hardchee.se>
2014-12-07 13:58:31 -0800

    Adding a helper to view trees

--- src/TrinaryTree.hs
+++ src/TrinaryTree.hs
@@ -25,6 +25,11 @@ insert y (TTree x left center right)
   | y >  x = TTree x left center (work right)
   where work = insert y
 
+trinaryTreeToList :: TrinaryTree a -> [a]
+trinaryTreeToList TEmpty = []
+trinaryTreeToList (TTree x left center right) = (trinaryTreeToList left) ++ [x] ++ center ++ (trinaryTreeToList right)
+
 main = do
   let x = foldl (flip insert) TEmpty [5, 4, 9, 5, 7, 2, 2]
+  putStrLn $ show $ trinaryTreeToList x
   putStrLn $ show x
